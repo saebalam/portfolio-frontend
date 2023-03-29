1ad0d77ea19917927139e30e985705e9c73947ef
saebalam <saebalam123@gmail.com>
2023-03-29 20:36:09 +0530

    new card

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,6 +3,7 @@ import './work.css'
 import Card from '../Shared Component/Card'
 import line from '../../Assets/Images/fancy-line.png'
 import frontpage_bookmania from '../../Assets/Images/homepage1.png'
+import admin_dashboard from '../../Assets/Images/admin_dashboard.png'
 
 const Work = () => {
     return ( 
@@ -13,7 +14,7 @@ const Work = () => {
 
             <div style={{padding:'10px',display:'flex',gap:'20px'}}>
                 <Card link='https://book-mania.netlify.app/' name='Bookmania' img={frontpage_bookmania} />
-                <Card link='https://book-mania.netlify.app/' name='Another' />
+                <Card link='https://admin-dashboard23.netlify.app/' name='Admin Dashboard' img={admin_dashboard} />
             </div>
 
         </div>
